80b6401f5b9b934bd021665f1216a30d15dbc962
mohaiminulnirob <mohaiminulislam20000@gmail.com>
2025-10-18 01:48:24 +0600

    add backend folder structure and server setup

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import mysql from "mysql2/promise";
+dotenv.config();
+
+export const connectDB = async () => {
+  try {
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    });
+    console.log("✅ MySQL connected successfully");
+    global.db = connection;
+  } catch (error) {
+    console.error("❌ Database connection failed:", error.message);
+  }
+};
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.get("/", (req, res) => res.send("FixMate Backend Running 🚀"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
