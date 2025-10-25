223c4c8c0eca99c592e8055ab2c5daaa357f58e0
Nirob <mohaiminulislam20000@gmail.com>
2025-10-26 05:51:14 +0600

    integrate worker route at backend server.js file

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,9 @@ import express from "express";
 import { connectDB } from "./config/db.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
+import ratingRoutes from "./routes/ratingRoutes.js";
 import serviceRoutes from "./routes/serviceRoutes.js";
 import workerRoutes from "./routes/workerRoutes.js";
-
 
 dotenv.config();
 const app = express();
@@ -16,9 +16,8 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/requests", serviceRoutes);
+app.use("/api/ratings", ratingRoutes);
 app.use("/api/workers", workerRoutes);
-
-
 connectDB();
 
 app.get("/", (req, res) => res.send("FixMate Backend Running 🚀"));
