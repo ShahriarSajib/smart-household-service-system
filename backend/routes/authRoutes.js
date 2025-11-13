22c0491a25f4e6a158cb81a873ecc2937fad0ee3
ShahriarSajib <sajibhossen516@gmail.com>
2025-11-13 13:51:40 +0600

    removing unnecessary stuffs from authRoutes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,8 +6,5 @@ const router = express.Router();
 router.post("/register/user", registerUser);
 router.post("/register/worker", registerWorker);
 router.post("/login", login);
-router.get("/test", (req, res) => {
-  res.send("Auth route working ✅");
-});
 
 export default router;
